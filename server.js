b24cd91d4b4bcc9442b59ce1849bdb3d370dded9
announcement7 <masterbruce2023@gmail.com>
2025-10-31 09:24:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,14 @@ const app = express();
 const PORT = 3000;
 
 // ✅ Firestore setup
-const serviceAccount = require("./serviceAccountKey.json");
+const admin = require("firebase-admin");
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 const db = admin.firestore();
 
